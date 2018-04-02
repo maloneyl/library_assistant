38312d3ee02fbf648590e4e171cbe7bde0d0ee86
Maloney L <maloneyl@users.noreply.github.com>
2018-04-02 13:43:39 +0100

    refactor .grab_a_book to be easier to grasp

--- lib/library_assistant.rb
+++ lib/library_assistant.rb
@@ -3,17 +3,17 @@ require "library_assistant/goodreads"
 
 module LibraryAssistant
   def self.grab_a_book
-    found = false
+    book = nil
+    book_requests = generate_book_requests
 
-    result = generate_book_requests.each do |request|
-      request.perform_library_search!
-
-      if found = request.book_found?
-        break request.library_search_result.book
-      end
+    while !book && book_requests.any?
+      book_request = book_requests.first
+      book_request.perform_library_search!
+      book = book_request.library_search_result.book
+      book_requests.delete(book_request)
     end
 
-    found ? result : nil
+    book
   end
 
   def self.generate_and_filter_book_requests
